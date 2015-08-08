f3a28627e52ca41af3029d03265e0afc04acf276
Adrian Sampson <adrian@radbox.org>
2015-08-08 15:29:51 -0700

    Error reporting

--- atw.ts
+++ atw.ts
@@ -1,6 +1,6 @@
 /// <reference path="typings/node/node.d.ts" />
-var fs = require('fs');
-var parser = require('./parser.js');
+let fs = require('fs');
+let parser = require('./parser.js');
 
 let fn = process.argv[2];
 if (!fn) {
@@ -13,5 +13,19 @@ fs.readFile(fn, function (err, data) {
     process.exit(1);
   }
   let s = data.toString();
-  console.log(parser.parse(s));
+
+  let tree;
+  try {
+    tree = parser.parse(s);
+  } catch (e) {
+    if (e instanceof parser.SyntaxError) {
+      console.log('parse error, ' + fn + ':' + e.line + ',' + e.column
+                  + ': ' + e.message);
+      process.exit(1);
+    } else {
+      throw e;
+    }
+  }
+
+  console.log(tree);
 });
