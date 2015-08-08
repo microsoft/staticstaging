77db9277031d61aadef1cf7affc7e68d51cf46a2
Adrian Sampson <adrian@radbox.org>
2015-08-08 15:51:12 -0700

    Encapsulate parse junk

--- atw.ts
+++ atw.ts
@@ -2,30 +2,38 @@
 let fs = require('fs');
 let parser = require('./parser.js');
 
+function parse(filename: string, f: (tree: any) => any) {
+  fs.readFile(filename, function (err, data) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    let s = data.toString();
+
+    let tree;
+    try {
+      tree = parser.parse(s);
+    } catch (e) {
+      if (e instanceof parser.SyntaxError) {
+        console.log('parse error at '
+                    + filename + ':' + e.line + ',' + e.column
+                    + ': ' + e.message);
+        process.exit(1);
+      } else {
+        throw e;
+      }
+    }
+
+    f(tree);
+  });
+}
+
 let fn = process.argv[2];
 if (!fn) {
   console.log("no input provided");
   process.exit(1);
 }
-fs.readFile(fn, function (err, data) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-  let s = data.toString();
 
-  let tree;
-  try {
-    tree = parser.parse(s);
-  } catch (e) {
-    if (e instanceof parser.SyntaxError) {
-      console.log('parse error, ' + fn + ':' + e.line + ',' + e.column
-                  + ': ' + e.message);
-      process.exit(1);
-    } else {
-      throw e;
-    }
-  }
-
+parse(fn, function (tree) {
   console.log(tree);
 });
