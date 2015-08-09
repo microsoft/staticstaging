4ad32c0afe14f19af41a12cfa150087855be995e
Adrian Sampson <adrian@radbox.org>
2015-08-09 12:46:45 -0700

    Thread through environment (heap)

--- interp.ts
+++ interp.ts
@@ -1,3 +1,5 @@
+// Types for AST nodes.
+
 interface SyntaxNode {
   tag: string;
 }
@@ -14,30 +16,45 @@ interface SeqNode extends ExpressionNode {
   rhs: ExpressionNode;
 }
 
+
+// Dynamic syntax.
+
+type Value = number;
+
 interface Env {
-  [key: string]: number;
+  [key: string]: Value;
 }
 
-function interpret_literal(tree: LiteralNode, env: Env) {
-  return tree.value;
+
+// Dynamic semantics rules.
+
+function interp_literal(tree: LiteralNode, env: Env): [Value, Env] {
+  return [tree.value, env];
 }
 
-function interpret_seq(tree: SeqNode, env: Env) {
-  interpret(tree.lhs, env);
-  return interpret(tree.rhs, env);
+function interp_seq(tree: SeqNode, env: Env): [Value, Env] {
+  let [v, e] = interp(tree.lhs, env);
+  return interp(tree.rhs, e);
 }
 
-// Dispatch based on tag. A somewhat messy alternative to constructing the AST
-// in a type-safe way, but it'll do.
-function interpret(tree: SyntaxNode, env: Env = {}): any {
+
+// Tag-based dispatch to the interpreter rules. A somewhat messy alternative
+// to constructing the AST in a type-safe way, but it'll do.
+function interp(tree: SyntaxNode, env): [Value, Env] {
   switch (tree.tag) {
     case "literal":
-      return interpret_literal(<LiteralNode> tree, env);
+      return interp_literal(<LiteralNode> tree, env);
     case "seq":
-      return interpret_seq(<SeqNode> tree, env);
+      return interp_seq(<SeqNode> tree, env);
 
     default:
       console.log("error: unknown syntax node " + tree.tag);
       return;
   }
+}
+
+// Helper to execute to completion in an empty initial environment.
+function interpret(program: SyntaxNode): Value {
+  let [v, e] = interp(program, {});
+  return v;
 }
