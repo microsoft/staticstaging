8d03963db5aec18c1ac84cd931e7db64c7c2cabb
Adrian Sampson <adrian@radbox.org>
2015-08-13 15:04:35 -0700

    Type is a little interface

--- src/type.ts
+++ src/type.ts
@@ -1,24 +1,37 @@
 /// <reference path="ast.ts" />
 /// <reference path="visit.ts" />
 
-// Wishing for algebraic data types.
-// type Type = BasicType | CodeType<T extends Type>;
-class Type { }
-class IntType extends Type { }
-class CodeType extends Type {
-  constructor(public valtype: Type) { super(); }
+interface Type {
+  tag: TypeTag,
+  stage: number,
+}
+
+enum TypeTag {
+  Int,
+}
+
+// These should probably be interned.
+function mktype(tag: TypeTag, stage: number = 0): Type {
+  return {
+    tag: tag,
+    stage: stage,
+  };
+}
+
+function pretty_type(t: Type): string {
+  return t.tag + "@" + t.stage;
 }
 
 interface TypeEnv {
   [key: string]: Type;
 }
 
-// "Enquote" all the values in an environment.
-function quote_env(e: TypeEnv): TypeEnv {
+// Adjust the stage of every type in an environment.
+function stage_env(e: TypeEnv, amount: number = 1): TypeEnv {
   let e2 : TypeEnv = {};
   for (let key in e) {
-    let typ : Type = e[key];
-    e2[key] = new CodeType(typ);
+    let t : Type = e[key];
+    e2[key] = mktype(t.tag, t.stage + amount);
   }
   return e2;
 }
@@ -28,7 +41,7 @@ function quote_env(e: TypeEnv): TypeEnv {
 
 let Typecheck : ASTVisit<TypeEnv, [Type, TypeEnv]> = {
   visit_literal(tree: LiteralNode, env: TypeEnv): [Type, TypeEnv] {
-    return [new IntType(), env];
+    return [mktype(TypeTag.Int), env];
   },
 
   visit_seq(tree: SeqNode, env: TypeEnv): [Type, TypeEnv] {
@@ -42,7 +55,7 @@ let Typecheck : ASTVisit<TypeEnv, [Type, TypeEnv]> = {
     // environment.
     let e2 = <TypeEnv> Object(e);
     e2[tree.ident] = t;
-    return [v, e2];
+    return [t, e2];
   },
 
   visit_lookup(tree: LookupNode, env: TypeEnv): [Type, TypeEnv] {
@@ -56,21 +69,29 @@ let Typecheck : ASTVisit<TypeEnv, [Type, TypeEnv]> = {
   visit_binary(tree: BinaryNode, env: TypeEnv): [Type, TypeEnv] {
     let [t1, e1] = typecheck(tree.lhs, env);
     let [t2, e2] = typecheck(tree.rhs, e1);
-    if (t1 instanceof IntType && t2 instanceof IntType) {
-      return [new IntType(), env];
+    if (t1.stage == 0 && t2.stage == 0) {
+      if (t1.tag == TypeTag.Int && t2.tag == TypeTag.Int) {
+        return [mktype(TypeTag.Int), env];
+      } else {
+        throw "type error: binary operation on non-numbers";
+      }
     } else {
-      throw "type error: binary operation on non-numbers";
+      throw "type error: binary operation on wrong stage";
     }
   },
 
   visit_quote(tree: QuoteNode, env: TypeEnv): [Type, TypeEnv] {
-    // TK
+    // Move the current context "up" before checking inside the quote.
+    let inner_env = stage_env(env, -1);
+    let [t, e] = typecheck(tree.expr, inner_env);
+    // And move the result type back "down".
+    return [mktype(t.tag, t.stage + 1), env];
   },
 
   visit_run(tree: RunNode, env: TypeEnv): [Type, TypeEnv] {
     let [t, e] = typecheck(tree.expr);
-    if (t instanceof CodeType) {
-      return [t.valtype, e];
+    if (t.stage > 0) {
+      return [mktype(t.tag, t.stage - 1), e];
     } else {
       throw "type error: running a non-code type";
     }
