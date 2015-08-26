1786664f01e0e76902edbe2890624c6de4462062
Adrian Sampson <adrian@radbox.org>
2015-08-26 13:52:48 -0700

    Finish most of quote interpreter

--- src/interp.ts
+++ src/interp.ts
@@ -91,7 +91,7 @@ function interp(tree: SyntaxNode, env: Env): [Value, Env] {
 
 // Another visitor for scanning over a quoted tree. Returns the tree
 // unchanged, except when it reaches an escape.
-// TODO Need to walk to children.
+// TODO Make a RecursiveASTVisit that encapsulates the walking?
 let QuoteInterp : ASTVisit<Env, [SyntaxNode, Env]> = {
   visit_literal(tree: LiteralNode, env: Env): [SyntaxNode, Env] {
     return [tree, env];
@@ -100,12 +100,14 @@ let QuoteInterp : ASTVisit<Env, [SyntaxNode, Env]> = {
   visit_seq(tree: SeqNode, env: Env): [SyntaxNode, Env] {
     let [t1, e1] = quote_interp(tree.lhs, env);
     let [t2, e2] = quote_interp(tree.rhs, e1);
-    let t : SeqNode = {tag: "seq", lhs: t1, rhs: t2};
-    return [t, e2];
+    let out : SeqNode = {tag: "seq", lhs: t1, rhs: t2};
+    return [out, e2];
   },
 
   visit_let(tree: LetNode, env: Env): [SyntaxNode, Env] {
-    return [tree, env];
+    let [t, e] = quote_interp(tree.expr, env);
+    let out : LetNode = {tag: "let", ident: tree.ident, expr: t};
+    return [out, e];
   },
 
   visit_lookup(tree: LookupNode, env: Env): [SyntaxNode, Env] {
@@ -113,6 +115,7 @@ let QuoteInterp : ASTVisit<Env, [SyntaxNode, Env]> = {
   },
 
   visit_quote(tree: RunNode, env: Env): [SyntaxNode, Env] {
+    // TODO
     return [tree, env];
   },
 
@@ -121,16 +124,21 @@ let QuoteInterp : ASTVisit<Env, [SyntaxNode, Env]> = {
     if (typeof v === "code") {
       return [v.expr, e];
     } else {
-      throw "error: escaped to non-code value " + v;
+      throw "error: escape produced non-code value " + v;
     }
   },
 
   visit_run(tree: RunNode, env: Env): [SyntaxNode, Env] {
-    return [tree, env];
+    let [t, e] = quote_interp(tree.expr, env);
+    let out : RunNode = {tag: "run", expr: t};
+    return [out, e];
   },
 
   visit_binary(tree: BinaryNode, env: Env): [SyntaxNode, Env] {
-    return [tree, env];
+    let [t1, e1] = quote_interp(tree.lhs, env);
+    let [t2, e2] = quote_interp(tree.rhs, e1);
+    let out : BinaryNode = {tag: "binary", op: tree.op, lhs: t1, rhs: t2};
+    return [out, e2];
   }
 }
 
