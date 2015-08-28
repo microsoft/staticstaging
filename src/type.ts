3add90111a2132daffc0788b5f092cf80a6a1180
Adrian Sampson <adrian@radbox.org>
2015-08-28 11:47:26 -0700

    Thread level through type checker

--- src/type.ts
+++ src/type.ts
@@ -53,18 +53,18 @@ function stage_env(e: TypeEnv, amount: number = 1): TypeEnv {
 
 // Type rules.
 
-let Typecheck : ASTVisit<TypeEnv, [Type, TypeEnv]> = {
-  visit_literal(tree: LiteralNode, env: TypeEnv): [Type, TypeEnv] {
+let Typecheck : ASTVisit<[TypeEnv, number], [Type, TypeEnv]> = {
+  visit_literal(tree: LiteralNode, [env, level]: [TypeEnv, number]): [Type, TypeEnv] {
     return [mktype(TypeTag.Int), env];
   },
 
-  visit_seq(tree: SeqNode, env: TypeEnv): [Type, TypeEnv] {
-    let [t, e] = check(tree.lhs, env);
-    return check(tree.rhs, e);
+  visit_seq(tree: SeqNode, [env, level]: [TypeEnv, number]): [Type, TypeEnv] {
+    let [t, e] = check(tree.lhs, env, level);
+    return check(tree.rhs, e, level);
   },
 
-  visit_let(tree: LetNode, env: TypeEnv): [Type, TypeEnv] {
-    let [t, e] = check(tree.expr, env);
+  visit_let(tree: LetNode, [env, level]: [TypeEnv, number]): [Type, TypeEnv] {
+    let [t, e] = check(tree.expr, env, level);
     // Like the interpreter, we abuse prototypes to create an overlay
     // environment.
     let e2 = <TypeEnv> Object.create(e);
@@ -72,7 +72,7 @@ let Typecheck : ASTVisit<TypeEnv, [Type, TypeEnv]> = {
     return [t, e2];
   },
 
-  visit_lookup(tree: LookupNode, env: TypeEnv): [Type, TypeEnv] {
+  visit_lookup(tree: LookupNode, [env, level]: [TypeEnv, number]): [Type, TypeEnv] {
     let t = env[tree.ident];
     if (t === undefined) {
       throw "type error: undefined variable " + tree.ident;
@@ -80,9 +80,9 @@ let Typecheck : ASTVisit<TypeEnv, [Type, TypeEnv]> = {
     return [t, env];
   },
 
-  visit_binary(tree: BinaryNode, env: TypeEnv): [Type, TypeEnv] {
-    let [t1, e1] = check(tree.lhs, env);
-    let [t2, e2] = check(tree.rhs, e1);
+  visit_binary(tree: BinaryNode, [env, level]: [TypeEnv, number]): [Type, TypeEnv] {
+    let [t1, e1] = check(tree.lhs, env, level);
+    let [t2, e2] = check(tree.rhs, e1, level);
     if (t1.stage == 0 && t2.stage == 0) {
       if (t1.tag == TypeTag.Int && t2.tag == TypeTag.Int) {
         return [mktype(TypeTag.Int), env];
@@ -94,24 +94,24 @@ let Typecheck : ASTVisit<TypeEnv, [Type, TypeEnv]> = {
     }
   },
 
-  visit_quote(tree: QuoteNode, env: TypeEnv): [Type, TypeEnv] {
+  visit_quote(tree: QuoteNode, [env, level]: [TypeEnv, number]): [Type, TypeEnv] {
     // Move the current context "up" before checking inside the quote.
     let inner_env = stage_env(env, -1);
-    let [t, e] = check(tree.expr, inner_env);
+    let [t, e] = check(tree.expr, inner_env, level + 1);
     // And move the result type back "down".
     return [mktype(t.tag, t.stage + 1), env];
   },
 
-  visit_escape(tree: EscapeNode, env: TypeEnv): [Type, TypeEnv] {
+  visit_escape(tree: EscapeNode, [env, level]: [TypeEnv, number]): [Type, TypeEnv] {
     // Move the context "down", in the opposite direction of quotation. Then
     // move the resulting type back "up".
     let inner_env = stage_env(env, 1);
-    let [t, e] = check(tree.expr, inner_env);
+    let [t, e] = check(tree.expr, inner_env, level - 1);
     return [mktype(t.tag, t.stage - 1), env];
   },
 
-  visit_run(tree: RunNode, env: TypeEnv): [Type, TypeEnv] {
-    let [t, e] = check(tree.expr, env);
+  visit_run(tree: RunNode, [env, level]: [TypeEnv, number]): [Type, TypeEnv] {
+    let [t, e] = check(tree.expr, env, level);
     if (t.stage > 0) {
       return [mktype(t.tag, t.stage - 1), e];
     } else {
@@ -120,11 +120,11 @@ let Typecheck : ASTVisit<TypeEnv, [Type, TypeEnv]> = {
   },
 }
 
-function check(tree: SyntaxNode, env: TypeEnv): [Type, TypeEnv] {
-  return ast_visit(Typecheck, tree, env);
+function check(tree: SyntaxNode, env: TypeEnv, level: number): [Type, TypeEnv] {
+  return ast_visit(Typecheck, tree, [env, level]);
 }
 
 function typecheck(tree: SyntaxNode): Type {
-  let [t, e] = check(tree, {});
+  let [t, e] = check(tree, {}, 0);
   return t;
 }
