c90cd24ff8766fc61674c2869c7da5dd1c7cddd3
Adrian Sampson <adrian@radbox.org>
2015-09-01 13:09:02 -0700

    Semi-functional dingus

--- dingus/atw.ts
+++ dingus/atw.ts
@@ -4,5 +4,48 @@
 
 declare var parser : any;
 
-console.log(interpret);
-console.log(parser);
+function atw_run(code: string) {
+  // Parse.
+  let tree: SyntaxNode;
+  try {
+    tree = parser.parse(code);
+  } catch (e) {
+    if (e instanceof parser.SyntaxError) {
+      console.log('parse error at '
+                  + e.line + ',' + e.column
+                  + ': ' + e.message);
+      return;
+    } else {
+      throw e;
+    }
+  }
+
+  // Log the parse tree.
+  try {
+    console.log(tree);
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+
+  // Log the type.
+  try {
+    console.log(pretty_type(typecheck(tree)));
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+
+  // Show the result value.
+  console.log(interpret(tree));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let codebox = <HTMLTextAreaElement> document.querySelector('textarea');
+  let runbtn = <HTMLButtonElement> document.querySelector('button');
+
+  runbtn.addEventListener('click', function () {
+    let code = codebox.value;
+    atw_run(code);
+  });
+});
