64b73656b70c278fdcf2203a47565a7b541b67e7
Adrian Sampson <adrian@radbox.org>
2015-09-16 11:23:51 -0700

    Check for expected type errors

--- test.sh
+++ test.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 fn=$1
 name=$2
+
 output=$(node atw.js $fn)
 expected=$(sed -n 's/^# -> \(.*\)/\1/p' $fn)
-if [ "$output" = "$expected" ] ; then
+
+if [ "$expected" = "type error" ] ; then
+    [[ "$output" == type\ error:* ]]
+else
+    [ "$output" = "$expected" ]
+fi
+success=$?
+
+if [ $success -eq 0 ] ; then
     echo $name ✓
 else
-    echo $name ✘: $output $expected
+    echo $name ✘: $output \($expected\)
 fi
