d14e82b917033a8bde88fd87b8f68a174b83b689
Adrian Sampson <adrian@radbox.org>
2015-09-22 17:56:05 -0700

    A simpler fixed-point combinator
    Exploits native JavaScript recursion. No need for the magic of the Y
    combinator.

--- src/util.ts
+++ src/util.ts
@@ -50,19 +50,12 @@ function cons<T> (x: T, xs: T[]): T[] {
 
 type Gen <T> = (_:T) => T;
 
-// The Y combinator. Adapted from Matt Might's JavaScript Y:
-// http://matt.might.net/articles/implementation-of-recursive-fixed-point-y-
-// combinator-in-javascript-for-memoization/
-// I added support for multiple-argument functions. I don't think it's
-// possible to typecheck this TypeScript, so it's quite unsafe.
+// A fixed-point combinator.
 function fix <T extends Function> (f : Gen<T>) : T {
-  return (function (x : any) {
-    return f(<any> function (...y: any[]) { return (x(x))(...y); });
-  })
-  (function (x : any) {
-    return f(<any> function (...y: any[]) { return (x(x))(...y); });
-  });
-};
+  return <any> function (...args: any[]) {
+    return (f(fix(f)))(...args);
+  };
+}
 
 // Function composition.
 function compose <A, B, C> (g : (_:B) => C, f : (_:A) => B): (_:A) => C {
