5d9cb79798dc9b1ef4387fe340692f17890d21c1
Adrian Sampson <adrian@radbox.org>
2015-09-24 14:15:42 -0700

    Copy ASTTranslator and add new nodes
    From revision: d800ce3902fdf95e003111ac0799d4e932ccee7d

--- src/sugar.ts
+++ src/sugar.ts
@@ -1,6 +1,81 @@
 /// <reference path="util.ts" />
 /// <reference path="ast.ts" />
 /// <reference path="type.ts" />
+/// <reference path="visit.ts" />
+
+// A visitor that traverses the AST recursively (in preorder) and creates a
+// copy of it. Override some of these functions to replace parts of the tree
+// with new SyntaxNodes.
+type ASTTranslate = ASTVisit<void, SyntaxNode>;
+let ASTTranslator : ASTTranslate = {
+  visit_literal(tree: LiteralNode, param: void): SyntaxNode {
+    return merge(tree);
+  },
+
+  visit_seq(tree: SeqNode, param: void): SyntaxNode {
+    return merge(tree, {
+      lhs: ast_visit(this, tree.lhs, null),
+      rhs: ast_visit(this, tree.rhs, null),
+    });
+  },
+
+  visit_let(tree: LetNode, param: void): SyntaxNode {
+    return merge(tree, {
+      expr: ast_visit(this, tree.expr, null),
+    });
+  },
+
+  visit_lookup(tree: LookupNode, param: void): SyntaxNode {
+    return merge(tree);
+  },
+
+  visit_binary(tree: BinaryNode, param: void): SyntaxNode {
+    return merge(tree, {
+      lhs: ast_visit(this, tree.lhs, null),
+      rhs: ast_visit(this, tree.rhs, null),
+    });
+  },
+
+  visit_quote(tree: QuoteNode, param: void): SyntaxNode {
+    return merge(tree, {
+      expr: ast_visit(this, tree.expr, null),
+    });
+  },
+
+  visit_escape(tree: EscapeNode, param: void): SyntaxNode {
+    return merge(tree, {
+      expr: ast_visit(this, tree.expr, null),
+    });
+  },
+
+  visit_run(tree: RunNode, param: void): SyntaxNode {
+    return merge(tree, {
+      expr: ast_visit(this, tree.expr, null),
+    });
+  },
+
+  visit_fun(tree: FunNode, param: void): SyntaxNode {
+    return merge(tree, {
+      body: ast_visit(this, tree.body, null),
+    });
+  },
+
+  visit_call(tree: CallNode, param: void): SyntaxNode {
+    let arg_trees : SyntaxNode[] = [];
+    for (let arg in tree.args) {
+      let translated_arg = <SyntaxNode> ast_visit(this, arg, null);
+      arg_trees.push(translated_arg);
+    }
+    return merge(tree, {
+      fun: ast_visit(this, tree.fun, null),
+      args: arg_trees,
+    });
+  },
+
+  visit_persist(tree: PersistNode, param: void): SyntaxNode {
+    throw "error: persist cannot appear in source code";
+  },
+};
 
 function is_lookup(tree: SyntaxNode): tree is LookupNode {
   return tree.tag === "lookup";
