f524ec4217f4690a4046d1be4ca83c4be3ce6443
Adrian Sampson <adrian@radbox.org>
2015-09-29 17:19:13 -0700

    Boilerplate for def/use mapping

--- src/compile.ts
+++ src/compile.ts
@@ -2,6 +2,65 @@
 /// <reference path="visit.ts" />
 /// <reference path="util.ts" />
 
+type DefUseTable = number[];
+type DefUseNameMap = { [name: string]: number };
+type FindDefUse = (tree: SyntaxNode, defs: DefUseNameMap) => [DefUseNameMap, DefUseTable];
+function gen_defuse(fself: FindDefUse): FindDefUse {
+  let def_use_rules : ASTVisit<DefUseNameMap, [DefUseNameMap, DefUseTable]> = {
+    visit_literal(tree: LiteralNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      return [defs, []];
+    },
+
+    visit_seq(tree: SeqNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      let [m1, t1] = fself(tree.lhs, defs);
+      let [m2, t2] = fself(tree.lhs, m1);
+      return [m2, t2];
+    },
+
+    visit_let(tree: LetNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      throw "unimplemented";
+    },
+
+    visit_lookup(tree: LookupNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      throw "unimplemented";
+    },
+
+    visit_binary(tree: BinaryNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      let [m1, t1] = fself(tree.lhs, defs);
+      let [m2, t2] = fself(tree.lhs, m1);
+      return [m2, t2];
+    },
+
+    visit_quote(tree: QuoteNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      throw "unimplemented";
+    },
+
+    visit_escape(tree: EscapeNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      throw "unimplemented";
+    },
+
+    visit_run(tree: RunNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      throw "unimplemented";
+    },
+
+    visit_fun(tree: FunNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      throw "unimplemented";
+    },
+
+    visit_call(tree: CallNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      throw "unimplemented";
+    },
+
+    visit_persist(tree: PersistNode, defs: DefUseNameMap): [DefUseNameMap, DefUseTable] {
+      throw "error: persist cannot appear in source";
+    },
+  }
+
+  return function(tree: SyntaxNode): [DefUseNameMap, DefUseTable] {
+    return ast_visit(def_use_rules, tree, {});
+  };
+}
+
 type JSCompile = (tree: SyntaxNode) => string;
 function gen_jscompile(fself: JSCompile): JSCompile {
   let compile_rules : ASTVisit<void, string> = {
