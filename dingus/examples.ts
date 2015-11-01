607f06f533c4ab159694975e818b6d22bc8fa975
Adrian Sampson <adrian@radbox.org>
2015-11-01 13:21:17 -0800

    Add Phong to dingus

--- dingus/examples.ts
+++ dingus/examples.ts
@@ -61,5 +61,52 @@ render r<
   >
 >
 `,
+},
+{
+name: "phong lighting shader",
+mode: "webgl",
+code: `
+var projection = dingus.projection;
+var model = dingus.model;
+var view = dingus.view;
+var position = bunny.positions;
+var normal = bunny.normals;
+
+var shininess = 0.5;
+
+render r<
+  var t = Date.now();
+  var lx = Math.sin(t / 200);
+  var ly = Math.sin(t / 100);
+  var lz = Math.sin(t / 300);
+  vtx s<
+    var light_position = vec3(lx, ly, lz);
+
+    var view_model = view * model;
+    var view_model_position = view_model * vec4(position, 1.0);
+
+    var camera_position = vec3(view_model_position);
+
+    gl_Position = projection * view_model_position;
+
+    # Convert to world space.
+    var position_world = vec3(model * vec4(position, 1.0));
+    var normal_world = normalize(vec3(model * vec4(position, 0.0)));
+    var view_direction = normalize(camera_position - position_world);
+
+    var light_direction = normalize(light_position - position_world);
+
+    var norm_norm = normalize(normal);
+
+    frag s<
+      # Phong power.
+      var r = -(reflect(light_direction, norm_norm));
+      var power = pow(max(0.0, dot(view_direction, r)), shininess);
+
+      gl_FragColor = vec4(power, power, power, 1.0);
+    >
+  >
+>
+`,
 }
 ];
