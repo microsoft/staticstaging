c2e8fd01c3d5e68d5cdf0e07c590494f8fa97712
Adrian Sampson <adrian@radbox.org>
2015-11-03 15:52:37 -0800

    Refactor context wrapper function

--- dingus/gl.js
+++ dingus/gl.js
@@ -23,22 +23,55 @@ function make_buffer(gl, data, type, mode) {
   return buf;
 }
 
-// TODO Remove this.
-// Given a mesh, with the fields `positions` and `cells`, create three buffers
-// for drawing the thing. Return an object with the fields:
-// - `cells`, a 3-dimensional uint16 element array buffer
-// - `positions`, a 3-dimensional float32 array buffer
-// - `normals`, ditto
-function mesh_buffers(gl, obj) {
-  var norm = normals.vertexNormals(obj.cells, obj.positions);
+// Evaluate the compiled SHFL code in the context of the globals we provide as
+// externs. Return a setup function that takes no arguments and returns a
+// per-frame function.
+function shfl_eval(code, gl, projection, model, view) {
+  var dingus = {
+    projection: projection,
+    model: model,
+    view: view,
+  };
 
-  return {
-    cells: make_buffer(gl, obj.cells, 'uint16', gl.ELEMENT_ARRAY_BUFFER),
-    positions: make_buffer(gl, obj.positions, 'float32', gl.ARRAY_BUFFER),
-    normals: make_buffer(gl, norm, 'float32', gl.ARRAY_BUFFER),
+  // Operations exposed to the language for getting data for meshes. These are
+  // curried so that the compiler can pass the `gl` parameter without exposing it
+  // to the program.
+  function mesh_indices(gl) {
+    return function(obj) {
+      return make_buffer(gl, obj.cells, 'uint16', gl.ELEMENT_ARRAY_BUFFER);
+    }
   }
-}
+  function mesh_positions(gl) {
+    return function(obj) {
+      return make_buffer(gl, obj.positions, 'float32', gl.ARRAY_BUFFER);
+    }
+  }
+  function mesh_normals(gl) {
+    return function(obj) {
+      var norm = normals.vertexNormals(obj.cells, obj.positions);
+      return make_buffer(gl, norm, 'float32', gl.ARRAY_BUFFER);
+    }
+  }
+  function mesh_size(gl) {
+    return function(obj) {
+      return obj.cells.length * obj.cells[0].length;
+    }
+  }
 
+  // And, similarly, a function for actually drawing a mesh. This takes the
+  // indices buffer for the mesh and its size (in the number of scalars).
+  function draw_mesh(gl) {
+    return function(indices, size) {
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indices);
+      gl.drawElements(gl.TRIANGLES, size, gl.UNSIGNED_SHORT, 0);
+    }
+  }
+
+  // Evaluate the code, but wrap it in a function to avoid scope pollution.
+  return (function () {
+    return eval(code);
+  })();
+}
 
 // Compute a project matrix (placed in the `out` matrix allocation) given the
 // width and height of a viewport.
@@ -64,10 +97,6 @@ function start_gl(container, shfl_code) {
   // Initialize the OpenGL context with our rendering function.
   var gl = glContext(canvas, render);
 
-  // Load the shape data into buffers.
-  var bunny_buffers = mesh_buffers(gl, bunny);
-  var teapot_buffers = mesh_buffers(gl, teapot);
-
   // Create the base matrices to be used
   // when rendering the bunny. Alternatively, can
   // be created using `new Float32Array(16)`
@@ -75,53 +104,8 @@ function start_gl(container, shfl_code) {
   var model = mat4.create();
   var view = mat4.create();
 
-  // Execute the compiled SHFL code with the "globals" we want to pass.
-  var shfl_program = (function (gl, bunny, teapot) {
-    var dingus = {
-      projection: projection,
-      model: model,
-      view: view,
-    };
-
-    // Operations exposed to the language for getting data for meshes. These are
-    // curried so that the compiler can pass the `gl` parameter without exposing it
-    // to the program.
-    function mesh_indices(gl) {
-      return function(obj) {
-        return make_buffer(gl, obj.cells, 'uint16', gl.ELEMENT_ARRAY_BUFFER);
-      }
-    }
-    function mesh_positions(gl) {
-      return function(obj) {
-        return make_buffer(gl, obj.positions, 'float32', gl.ARRAY_BUFFER);
-      }
-    }
-    function mesh_normals(gl) {
-      return function(obj) {
-        var norm = normals.vertexNormals(obj.cells, obj.positions);
-        return make_buffer(gl, norm, 'float32', gl.ARRAY_BUFFER);
-      }
-    }
-    function mesh_size(gl) {
-      return function(obj) {
-        return obj.cells.length * obj.cells[0].length;
-      }
-    }
-
-    // And, similarly, a function for actually drawing a mesh. This takes the
-    // indices buffer for the mesh and its size (in the number of scalars).
-    function draw_mesh(gl) {
-      return function(indices, size) {
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indices);
-        gl.drawElements(gl.TRIANGLES, size, gl.UNSIGNED_SHORT, 0);
-      }
-    }
-
-    // Evaluate the code, but wrap it in a function to avoid scope pollution.
-    return (function () {
-      return eval(shfl_code);
-    })();
-  })(gl, bunny, teapot);
+  // Execute the compiled SHFL code in context.
+  var shfl_program = shfl_eval(shfl_code, gl, projection, model, view);
 
   // Invoke the setup stage.
   var shfl_func = shfl_program();
