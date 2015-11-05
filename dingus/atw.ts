e15f50ea1f3e1257ca612a2643a679daa6a8bb71
Adrian Sampson <adrian@radbox.org>
2015-11-05 12:13:51 -0800

    Correct specular?

--- dingus/atw.ts
+++ dingus/atw.ts
@@ -148,6 +148,7 @@ extern mat4.scale: Mat4 Mat4 Vec3 -> Void;
 extern mat4.translate: Mat4 Mat4 Vec3 -> Void;
 extern mat4.transpose: Mat4 Mat4 -> Void;
 extern mat4.scale: Mat4 Mat4 Vec3 -> Void;
+extern mat4.invert: Mat4 Mat4 -> Void;
 
 extern Date.now: -> Float;
 extern Math.sin: Float -> Float;
