2b877309fad1dd1c49a3c99367d54999fe3c1be6
Adrian Sampson <adrian@radbox.org>
2015-11-05 10:44:30 -0800

    Scalar-vector constructors

--- src/backend_webgl.ts
+++ src/backend_webgl.ts
@@ -85,10 +85,12 @@ const GL_INTRINSICS: TypeMap = {
   vec4: new OverloadedType([
     new FunType([FLOAT3, FLOAT], FLOAT4),
     new FunType([FLOAT, FLOAT, FLOAT, FLOAT], FLOAT4),
+    new FunType([FLOAT], FLOAT4),
   ]),
   vec3: new OverloadedType([
     new FunType([FLOAT4], FLOAT3),
     new FunType([FLOAT, FLOAT, FLOAT], FLOAT3),
+    new FunType([FLOAT], FLOAT3),
   ]),
   abs: _GL_UNARY_TYPE,
   normalize: _GL_UNARY_TYPE,
