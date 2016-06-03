bc21d199cc4bde7d82350f1be0d11a1a6dcbc9f9
Adrian Sampson <adrian@radbox.org>
2016-06-03 12:49:13 -0700

    deploy [ci skip]

---
+++ main.js
@@ -0,0 +1,69 @@
+/**
+ * Load the content from an HTML <template> element by its selector.
+ */
+function getTemplate(selector) {
+    var tmpl = document.querySelector(selector);
+    return tmpl.content;
+}
+/**
+ * Replace a node in the DOM.
+ */
+function replace(new_, old) {
+    old.parentNode.replaceChild(new_, old);
+}
+/**
+ * Instantiate a template.
+ */
+function instantiate(tmpl) {
+    return document.importNode(tmpl, true);
+}
+/**
+ * Find a separator line, ---, in a code listing and separate it into the code
+ * *before* that line and the code *after* it. We use this to separate the
+ * code we show to the user from a preamble.
+ */
+function split_code(s) {
+    var index = s.search(/---\n/);
+    if (index === -1) {
+        // No marker: put all the code in the second half.
+        return ['', s.trim()];
+    }
+    else {
+        var front = s.slice(0, index);
+        front = front.slice(0, front.lastIndexOf("\n"));
+        var back = s.slice(index);
+        back = back.slice(back.indexOf("\n"));
+        return [front.trim(), back.trim()];
+    }
+}
+/**
+ * Replace a code block in the HTML with a dingus that initially contains the
+ * same code.
+ */
+function dingusify(orig, tmpl) {
+    // Set up the dingus.
+    var dingusEl = instantiate(tmpl);
+    var dingus = sscDingus(dingusEl, {
+        history: false,
+        lineNumbers: false,
+        scrollbars: false
+    });
+    // Fill in the code.
+    var code = orig.textContent.trim();
+    var _a = split_code(code), front = _a[0], back = _a[1];
+    dingus.set_preamble(front);
+    dingus.run(back, "webgl");
+    // Replace the old element with the new dingus.
+    replace(dingusEl, orig);
+    dingus.redraw();
+}
+document.addEventListener("DOMContentLoaded", function () {
+    // Load the dingus template.
+    var dingusTmpl = getTemplate('#template-dingus');
+    // Transform each code block into a dingus.
+    var pres = document.querySelectorAll('pre');
+    for (var i = 0; i < pres.length; ++i) {
+        var pre = pres[i];
+        dingusify(pre, dingusTmpl);
+    }
+});
