6a229187eba5af8ac8c6f5d843ca47baf00ade35
Adrian Sampson <adrian@radbox.org>
2016-06-03 10:34:17 -0700

    Replace <pre> elements with dingus

--- site/index.md
+++ site/index.md
@@ -6,3 +6,5 @@ An introduction goes here.
 ## Let's Draw Something
 
 An example here.
+
+    12 + 34
--- site/main.ts
+++ site/main.ts
@@ -1,7 +1,47 @@
 declare function sscDingus(el: any, config: any);
 
+/**
+ * Load the content from an HTML <template> element by its selector.
+ */
+function getTemplate(selector: string) {
+  let tmpl = document.querySelector(selector) as HTMLTemplateElement;
+  return tmpl.content;
+}
+
+/**
+ * Replace a node in the DOM.
+ */
+function replace(new_: Node, old: Element) {
+  old.parentNode.replaceChild(new_, old);
+}
+
+/**
+ * Instantiate a template.
+ */
+function instantiate(tmpl: DocumentFragment): Node {
+  return document.importNode(tmpl, true);
+}
+
+function dingusify(orig: Element, tmpl: DocumentFragment) {
+  let code = orig.textContent;
+  console.log(code);
+
+  let dingus = instantiate(tmpl);
+  replace(dingus, orig);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  var base = document.querySelector('.sscdingus');
+  // Load the dingus template.
+  let dingusTmpl = getTemplate('#template-dingus');
+
+  // Transform each code block into a dingus.
+  let pres = document.querySelectorAll('pre');
+  for (let i = 0; i < pres.length; ++i) {
+    let pre = pres[i];
+    dingusify(pre, dingusTmpl);
+  }
+
+  let base = document.querySelector('.sscdingus');
   sscDingus(base, {
     history: false,
     lineNumbers: false,
