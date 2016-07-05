6a73b966fff2ed7aa94cd205abdc0b4a607ffe1a
Adrian Sampson <adrian@radbox.org>
2016-07-05 16:58:19 -0700

    Serve the dingus
    *Insanely*, the `serveStatic` middleware from the node-restify dumpster fire
    has no way to mount a directory on a path---this must be at the root.

--- harness/harness.ts
+++ harness/harness.ts
@@ -3,11 +3,20 @@ const restify = require('restify');
 function serve() {
   let server = restify.createServer();
   let qp = restify.queryParser({ mapParams: false });
+
+  // Log messages to a file.
   server.get('/log', qp, (req: any, res: any, next: any) => {
     console.log(req.query['msg']);
     res.send('done');
     next();
   });
+
+  // Serve the dingus assets.
+  server.get(/\/.*/, restify.serveStatic({
+    directory: '../dingus',
+    default: 'index.html',
+  }));
+
   server.listen(4700, () => {
     console.log("listening on %s", server.url);
   });
