6ab28e223a75de80725ebcfb8a598d8aa4fc68cd
Adrian Sampson <adrian@radbox.org>
2016-07-05 15:55:47 -0700

    Fix type errors in harness

--- harness/harness.ts
+++ harness/harness.ts
@@ -3,7 +3,7 @@ const restify = require('restify');
 function serve() {
   let server = restify.createServer();
   let qp = restify.queryParser({ mapParams: false });
-  server.get('/log', qp, (req, res, next) => {
+  server.get('/log', qp, (req: any, res: any, next: any) => {
     console.log(req.query['msg']);
   });
   server.listen(4700, () => {
