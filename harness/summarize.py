7603eb7090785e0e6dc99e49079a810093ecf2cc
Adrian Sampson <adrian@radbox.org>
2016-07-07 16:24:55 -0700

    Compute frame latency

--- harness/summarize.py
+++ harness/summarize.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
 import os
 import json
+import math
 
 TIMINGS_DIR = 'collected'
 
 
-def mean(values):
+def _mean(values):
+    """The arithmetic mean."""
     return sum(values) / len(values)
 
 
-def summarize_run(data):
-    print(data['fn'])
-    for msg in data['messages']:
-        frames = msg['frames']
-        elapsed = msg['ms']
-        latencies = msg['latencies']
+def _mean_err(vals):
+    """The mean and standard error of the mean."""
+    if len(vals) <= 1:
+        return 0.0
+    mean = _mean(vals)
+    stdev = math.sqrt(sum((x - mean) ** 2 for x in vals) / (len(vals) - 1))
+    return mean, stdev / math.sqrt(len(vals))
 
-        print(latencies)
-        print('simple', elapsed / frames)
-        print('complicated', mean(latencies))
+
+def summarize_run(data):
+    """Summarize the data from a single run."""
+    print(data['fn'])
+    all_latencies = []
+    for msg in data['messages']:
+        # As a sanity check, we can get an average frame latency for the
+        # entire message with:
+        # avg_latency = msg['ms'] / msg['frames']
+
+        latencies = msg['latencies']
+        all_latencies += latencies
+
+        # TODO Skip the first message as a "warmup" period.
+
+    mean, err = _mean_err(latencies)
+    print('frame latency:', mean, '+/-', err, 'ms')
 
 
 def summarize():
+    """Sumarrize all the collected data."""
     for fn in os.listdir(TIMINGS_DIR):
         path = os.path.join(TIMINGS_DIR, fn)
         with open(path) as f:
